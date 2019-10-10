1823d839f268fbb1e722f0f1d26444f2fac187de
Bertrand Thierry <bthierry@math.cnrs.fr>
2019-10-10 11:10:57 +0200

    move shortcodes to layouts/

--- update_thm.sh
+++ update_thm.sh
@@ -5,7 +5,7 @@
 # Keep this file to update it!
 mkdir -p layouts/partials/shortcodes i18n assets/css
 git clone https://github.com/Bertbk/hugo-thm.git
-cp -r hugo-thm/layouts/partials/shortcodes/thm layouts/partials/shortcodes/thm
+cp -r hugo-thm/layouts/shortcodes/thm layouts/shortcodes/thm
 cp -r hugo-thm/i18n/thm i18n/thm
 cp hugo-thm/assets/css/thm.css assets/css/thm.css
 rm -rf hugo-thm
